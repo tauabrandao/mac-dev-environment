c27f00f018309a6023a2ff0b1b58fab8e5256069
tauabrandao <brandao.taua@gmail.com>
2023-03-01 16:01:04 -0500

    uninstall environment

---
+++ uninstall-dev-environment.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Uninstall JDK 11 and 16
+echo "Uninstalling JDKs..."
+brew uninstall --cask adoptopenjdk/openjdk/adoptopenjdk11
+brew uninstall --cask adoptopenjdk/openjdk/adoptopenjdk16
+
+# Uninstall Maven
+echo "Uninstalling Maven..."
+brew uninstall maven@3.9
+
+# Uninstall Docker
+echo "Uninstalling Docker..."
+brew uninstall --cask docker
+
+# Uninstall Minikube
+echo "Uninstalling Minikube..."
+brew uninstall minikube
+
+# Uninstall kubectl
+echo "Uninstalling kubectl..."
+brew uninstall kubectl
+
+# Uninstall DBeaver
+echo "Uninstalling DBeaver..."
+brew uninstall --cask dbeaver-community
+
+# Uninstall Postman
+echo "Uninstalling Postman..."
+brew uninstall --cask postman
+
+# Uninstall VSCode
+echo "Uninstalling Visual Studio Code..."
+brew uninstall --cask visual-studio-code
+
+# Uninstall Homebrew
+echo "Uninstalling Homebrew..."
+/bin/bash -c "$(curl -fsSL 
+https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+
+echo "Uninstallation complete."
+
